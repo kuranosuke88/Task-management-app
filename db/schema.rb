8f8aa9e7f83581f7cf08d68275fd3b703d44707e
EC2 Default User <ec2-user@ip-172-31-23-173.us-east-2.compute.internal>
2019-10-21 02:50:41 +0000

    M追加終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190723133958) do
+ActiveRecord::Schema.define(version: 20191021020332) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20190723133958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "detail"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
