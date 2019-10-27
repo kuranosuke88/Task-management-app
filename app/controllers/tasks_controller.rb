03cd77fb2065c54af1be4e4bfc7e4cd18006fd23
EC2 Default User <ec2-user@ip-172-31-23-173.us-east-2.compute.internal>
2019-10-27 01:38:44 +0000

    タスク新規作成解決

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
   private
   
    def task_params
-     params.require(:task).permit(:name, :description)
+     params.require(:task).permit(:name, :detail)
    end
 
 end
