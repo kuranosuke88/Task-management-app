fe6dc39342d300a18832d14af6de35669b879077
EC2 Default User <ec2-user@ip-172-31-23-173.us-east-2.compute.internal>
2019-11-06 14:18:13 +0000

    D6クリア

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
   end
   
   def index
+    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
   
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191021020332) do
+ActiveRecord::Schema.define(version: 20191106133406) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20191021020332) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
